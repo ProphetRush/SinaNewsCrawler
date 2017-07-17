d6fc302757c157d92e27f61f6d107e271ba0f9eb
ProphetRush <LIAOSIZHE@Gmail.com>
2017-07-18 00:29:08 +0800

    Crawler?111

--- sinaNewsCrawler.py
+++ sinaNewsCrawler.py
@@ -2,13 +2,13 @@ import requests
 from bs4 import BeautifulSoup
 import json
 import time
-
-
-
+import csv
+import collections
+import matplotlib.pyplot as plt
 
 def run():
-    show_num = 30
-    page = 15
+    show_num = 25
+    page = 40
     newsList = list()
     for i in range(page):
         url = "http://feed.mix.sina.com.cn/api/roll/get?pageid=204&lid=22&num=" + str(show_num) + "&versionNumber=1.2.8" \
@@ -16,13 +16,18 @@ def run():
         content = getJsonReturned(url)
         for k in content:
             newsList.append(k)
+    f = csv.writer(open("news.csv", "w+", encoding="utf"))
+    #newsJSON = json.loads(json.dumps(newsList))
+    f.writerow(["title", "articleKeyword", "source", "commentCount", "publishTime", "url"])
+    for x in newsList:
+        f.writerow([x["title"], x["articleKeyword"], x["source"], x["commentCount"], x["publishTime"], x["url"]])
     return newsList
 
 
 def getJsonReturned(url):
     newsList = list()
     newsInfos = (requests.get(url, timeout=30).json())['result']['data']
-    for i in range(30):
+    for i in range(25):
         newsInfo = dict()
         newsInfo["title"] = newsInfos[i]["title"]
         newsInfo["url"] = newsInfos[i]["url"]
@@ -37,7 +42,39 @@ def getJsonReturned(url):
         newsList.append(newsInfo)
     return newsList
 
-print(run())
+
+def wordCount(newsList):
+    keywords = list()
+    for x in newsList:
+        kw = x["articleKeyword"].split(",")
+        for i in kw:
+            keywords.append(i)
+    kw = collections.Counter(keywords)
+    s = sorted(kw.items(), key=lambda k: k [1], reverse=True)[:20]
+    return s
+
+
+def sourceCount(newsList):
+    sources = list()
+    for x in newsList:
+        sources.append(x["source"])
+    srcCount = collections.Counter(sources)
+    scount = sorted(srcCount.items(), key=lambda k: k[1], reverse=True)[:10]
+    return scount
+
+
+def drawTimeDistribution(newsList):
+    timePeriod = list()
+    for x in newsList:
+        timePeriod.append(x["publishTime"][11:13])
+    return collections.Counter(timePeriod)
+
+
+
+
+a = run()
+print(drawTimeDistribution(a))
+
 
 
 
